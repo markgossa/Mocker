1ec891ea136ed29851baa84efb0a5e6b449d7a8e
Mark Gossa <16326726+markgossa@users.noreply.github.com>
2020-07-29 20:54:16 +0100

    Removes SP vars

--- deployment/terraform/provider.tf
+++ deployment/terraform/provider.tf
@@ -1,8 +1,4 @@
 provider "azurerm" {
     version = "~> 2.2"
-    client_id       = var.client_id
-    client_secret   = var.client_secret
-    subscription_id = var.subscription_id
-    tenant_id       = var.tenant_id
     features {}
 }
--- deployment/terraform/variables.tf
+++ deployment/terraform/variables.tf
@@ -1,5 +1,1 @@
 variable "ip_address" {}
-variable "subscription_id" {}
-variable "client_id" {}
-variable "client_secret" {}
-variable "tenant_id" {}
